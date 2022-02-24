3fab1802bfb1a460b8741f22e6cb9f80d5c177c3
Olga <49266934+olgakos@users.noreply.github.com>
2022-02-24 19:35:04 +0300

    Step01: Add 2 Simple + 1 Parameterized Tests

---
+++ src/test/java/olgakos/github/yusupovPalaceParameterizedTests.java
@@ -0,0 +1,57 @@
+package olgakos.github;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Configuration.browserSize;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+@DisplayName("Класс c параметризованными тестами сайта Юсуповский дворец")
+public class yusupovPalaceParameterizedTests {
+    @BeforeEach
+    void preconditionBrowser() {
+        baseUrl = "https://www.yusupov-palace.ru/ru";
+        browserSize = "1920x1080";
+    }
+    @AfterEach
+    void closeBrowser() {
+        Selenide.closeWebDriver();
+    }
+
+    @CsvSource(value = {
+            "Феликс Юсупов| Имение Кореиз на Южном берегу Крыма",
+            "Дмитрий Павлович| из великих князей Дома Романовых"
+    }, delimiter = '|')
+    @ParameterizedTest(name = "Проверка отображения поисковых результатов для запроса \"{0}\"")
+    void searchTwoNewsTest (String testData, String expectedText){
+        //открыть страницу Экскурсионные программы
+        Selenide.open("/programs");
+        alertWindowMethod(); //алерт-окно про "QR-код"
+        //проверяем заголовок cтраницы
+        $(".logo-title").shouldHave(text("Юсуповский дворец"));
+        //найти поле "поиск" + ввести запрос
+        $(".search").setValue(testData).pressEnter();
+        //откроется страница с заголовком "Результаты поиска"
+        $(".title").shouldHave(text("Результаты поиска"));
+        //на странице пристутсвует текст:
+        //$("body").shouldHave(text(expectedText));
+        $$("body").find(text(expectedText)).shouldBe(visible);
+    }
+
+    void alertWindowMethod (){
+        //ЕСЛИ есть алерт с текстом "необходимо предъявить QR-код":
+        if ($x("//h3[contains(.,'необходимо предъявить QR-код')]").is(exist)) {
+            //ТО кликни закрыть "х"
+            $(".closeAnnounce").click();
+            //ИНАЧЕ, пропускаем и
+            // сразу проверка заголовка:
+        }
+    }
+}
---
+++ src/test/java/olgakos/github/yusupovPalaceSimpleTests.java
@@ -0,0 +1,84 @@
+package olgakos.github;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Configuration.browserSize;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+@DisplayName("Класс с базовыми тестами сайта Юсуповский дворец")
+public class yusupovPalaceSimpleTests {
+
+    @BeforeEach
+    void preconditionBrowser() {
+        baseUrl = "https://www.yusupov-palace.ru/ru";
+        browserSize = "1920x1080";
+    }
+
+    @AfterEach
+    void closeBrowser() {
+        Selenide.closeWebDriver();
+    }
+
+    @Test
+    @DisplayName("News's search test")
+    void searchNewsTest() {
+        //открыть страницу Экскурсионные программы
+        Selenide.open("/programs");
+
+        //алерт-окно про "QR-код"
+        //ЕСЛИ есть алерт с текстом "необходимо предъявить QR-код":
+        if ($x("//h3[contains(.,'необходимо предъявить QR-код')]").is(exist)) {
+            //ТО кликни закрыть "х"
+            $(".closeAnnounce").click();
+        }
+        //ИНАЧЕ, пропускаем и сразу:
+        //проверяем заголовок страницы
+        $(".logo-title").shouldHave(text("Юсуповский дворец"));
+        //найти поле "поиск" + ввести запрос
+        $(".search").setValue("Феликс Юсупов").pressEnter();
+        //откроется страница с заголовком Результаты поиска
+        $(".title").shouldHave(text("Результаты поиска"));
+        //на странице пристутсвует текст:
+        $("body").shouldHave(text("Имение Кореиз на Южном берегу Крыма"));
+        $("body").shouldHave(text("Мать и сын Юсуповы: «Я тебя люблю с восхищением»"));
+
+        //не сработает прави, НЕ покажет ошибку:
+        //$(byText("Имение Коре1з")).isDisplayed();
+    }
+
+    @Test
+    @DisplayName("Form's error test")
+    void servicesFormTest() {
+        //открыть страницу
+        //Selenide.open("/services/presentation");
+        Selenide.open("https://www.yusupov-palace.ru/ru/services/presentation");
+
+        //ЕСЛИ есть алерт с текстом "необходимо предъявить QR-код":
+        if ($x("//h3[contains(.,'необходимо предъявить QR-код')]").is(exist)) {
+            //ТО кликни закрыть "х"
+            $(".closeAnnounce").click();
+        }
+        //ИНАЧЕ, пропускаем и
+        //проверяем заголовок cтраницы:
+
+        $("#servicesFormDiv").shouldHave(text("Форма заявки"));
+        $("#fio").setValue("Example Name").pressEnter();
+        $("#phone").setValue("Example Tel TEXT!").pressEnter();
+        $("#type").setValue("цель мероприятия").pressEnter();
+        $("#guests").setValue("два").pressEnter(); //ожаидается ошибка: int=2
+        $("[type=submit]").click(); // отправить
+
+        //Ожидаемый результат: появитcя ОКНО с текстом: "Пожалуйства, введите число"
+        $(byText("Пож1луйства, введиет число")).isDisplayed(); //тест зеленый, а должен быть красный
+        $x("//body[contains(.,'Пож1луйста, введите число')]").is(exist); //тест зеленый, а должен быть красный
+        $x("//text[contains(.,'Пож1луйста, введите число')]").isDisplayed(); //тест зеленый, а должен быть красный
+    }
+}
